14585874f318a98f2df640182d511971872d13ef
wendellucas <wendellucaslrd@gmail.com>
2024-09-03 14:09:13 -0300

    Add files via upload

--- php.php
+++ php.php
@@ -2,11 +2,19 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = $_POST['name'];
   $email = $_POST['email'];
-  $message = $_POST['message'];
+  $fone = $_POST['fone'];
+  $level = $_POST['level'];
 
   // Aqui você pode processar os dados, enviar email, salvar no banco de dados, etc.
 
-  echo "Formulário enviado com sucesso!";
+  $to = "wendellucaslrd@gmail.com";
+  $subject = "Novo cadastro por site para o método YOU CAN SPEAK";
+  $body = "Nome: $name\nEmail: $email\nTelefone:\n$fone\nNível de inglês: $level";
+  if (mail($to, $subject, $body)) {
+    echo "Email enviado com sucesso!";
+  } else {
+    echo "Erro ao enviar email.";
+  }
 } else {
   echo "Método de requisição inválido.";
 }
