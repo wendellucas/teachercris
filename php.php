28a79703554e546831d704f11a08bb0f2cde614f
wendellucas <wendellucaslrd@gmail.com>
2024-09-03 15:17:50 -0300

    Add files via upload

--- php.php
+++ php.php
@@ -1,21 +1,22 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $fone = $_POST['fone'];
-  $level = $_POST['level'];
+// if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//   $name = $_POST['name'];
+//   $email = $_POST['email'];
+//   $fone = $_POST['fone'];
+//   $level = $_POST['level'];
 
-  // Aqui você pode processar os dados, enviar email, salvar no banco de dados, etc.
+//   // Aqui você pode processar os dados, enviar email, salvar no banco de dados, etc.
 
-  $to = "wendellucaslrd@gmail.com";
-  $subject = "Novo cadastro por site para o método YOU CAN SPEAK";
-  $body = "Nome: $name\nEmail: $email\nTelefone:\n$fone\nNível de inglês: $level";
-  if (mail($to, $subject, $body)) {
-    echo "Email enviado com sucesso!";
-  } else {
-    echo "Erro ao enviar email.";
-  }
-} else {
-  echo "Método de requisição inválido.";
-}
+//   $to = "wendellucaslrd@gmail.com";
+//   $subject = "Novo cadastro por site para o método YOU CAN SPEAK";
+//   $body = "Nome: $name\nEmail: $email\nTelefone:\n$fone\nNível de inglês: $level";
+//   if (mail($to, $subject, $body)) {
+//     echo "Email enviado com sucesso!";
+//   } else {
+//     echo "Erro ao enviar email.";
+//   }
+// } else {
+//   echo "Método de requisição inválido.";
+// }
+echo "PHP";
 ?>
